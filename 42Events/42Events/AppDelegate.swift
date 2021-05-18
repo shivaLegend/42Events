8d30d76a4021be3604b3594ba55cf76e27e02c82
Tai Nguyen <nguyenductai.job@gmail.com>
2021-05-18 08:59:27 +0700

    init skeleton

---
+++ 42Events/42Events/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  42Events
+//
+//  Created by Tai Nguyen on 17/05/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
