2bf871651235c5cc7800c2d89b76cd0137da5bc2
Tai Nguyen <nguyenductai.job@gmail.com>
2021-05-20 13:10:27 +0700

    refactor

--- 42Events/42Events/EventViewController.swift
+++ 42Events/42Events/EventViewController.swift
@@ -11,7 +11,7 @@ class EventViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var totalEventsLbl: UILabel!
-    private let heightOfCell: CGFloat = 280
+    private let heightOfCell: CGFloat = 270
     private var data: DetailEvent?
     var typeEvent: TypeEvent = .Cycling
     override func viewDidLoad() {
